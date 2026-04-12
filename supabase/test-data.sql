cd6e510b0f42dd655a8a7a04c1056bc1f0bc0c47
Kamogelo-Kedige <kedigekamogelo32@gmail.com>
2026-04-12 11:16:27 +0200

    Seed database
    added data to seed the database

---
+++ supabase/test-data.sql
@@ -0,0 +1,5 @@
+-- TEST DATA
+insert into public.projects (title, description, stage, support_required, is_complete, user_id)
+values
+('Portfolio Website', 'Building my personal portfolio site', 'building', 'Need UI feedback', false, '24262f69-7951-472e-9c03-d5ddf7988fc7'),
+('Task Tracker App', 'A small productivity app for students', 'completed', 'No support needed', true, '5450bdaf-b76f-41f9-8e94-8ea0101eb889');
